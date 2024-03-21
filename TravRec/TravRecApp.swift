b4ab67cf7af72104535c9e2c3b5255751f04c828
hyen <jhi2359@naver.com>
2024-03-21 11:50:46 +0900

    Initial Commit

---
+++ TravRec/TravRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravRecApp.swift
+//  TravRec
+//
+//  Created by jeong hyein on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TravRecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
