0a039561dd3811f996db546ea1f01e578eb3aa10
hyen <jhi2359@naver.com>
2024-03-24 23:18:29 +0900

    community view

---
+++ TravRec/CommunityView.swift
@@ -0,0 +1,127 @@
+import SwiftUI
+
+struct CommunityView: View {
+    var userList: [userInterface] = [user1, user2, user3]
+    
+    var body: some View {
+        ScrollView {
+            Divider()
+            
+            ForEach(userList, id: \.id) {
+                user in
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image(user.userProfileImg)
+                            .resizable()
+                            .clipShape(Circle())
+                            .frame(width: 60, height: 60)
+                            .padding(.trailing, 15)
+                        VStack (alignment: .leading) {
+                            Text(user.userName)
+                                .font(.subheadline)
+                            Text(user.travelList[0].title)
+                                .font(.caption)
+                        }
+                        Spacer()
+                        Image(systemName: "bookmark")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20)
+                    }
+                    
+                    
+                    Divider()
+                        .padding(.bottom, 20)
+                        .padding(.top, 10)
+                    
+                    Group {
+                        Label("Bucket", systemImage: "checklist")
+                            .font(.title3)
+                            .foregroundColor(.gray)
+                        
+                        HStack {
+                            Spacer()
+                            VStack(alignment: .leading, spacing: 10) {
+                                ForEach(user.travelList[0].bucketList, id: \.id) {bucket in
+                                    HStack {
+                                        bucket.isComplete ? Image(systemName: "checkmark.circle.fill") : Image(systemName: "checkmark.circle")
+                                        Text(bucket.bucketTitle)
+                                    }
+                                    
+                                }
+                            }
+                            .padding(.vertical, 30)
+                            Spacer()
+                        }
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(.placeholder, lineWidth: 1)
+                        )
+                        .padding(.bottom, 10)
+                    }
+                    
+                    Group {
+                        Label("Happy", systemImage: "hand.thumbsup")
+                            .font(.title3)
+                            .foregroundColor(.gray)
+                        
+                        HStack {
+                            Spacer()
+                            VStack(alignment: .leading, spacing: 10) {
+                                ForEach(user.travelList[0].happyList!, id: \.self) {happy in
+                                    HStack {
+                                        Image(systemName: "checkmark.circle.fill")
+                                        Text(happy)
+                                    }
+                                    
+                                }
+                            }
+                            .padding(.vertical, 30)
+                            Spacer()
+                        }
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(.placeholder, lineWidth: 1)
+                        )
+                        .padding(.bottom, 10)
+                    }
+                    
+                    Group {
+                        Label("Sad", systemImage: "hand.thumbsdown")
+                            .font(.title3)
+                            .foregroundColor(.gray)
+                        
+                        HStack {
+                            Spacer()
+                            VStack(alignment: .leading, spacing: 10) {
+                                ForEach(user.travelList[0].sadList!, id: \.self) {sad in
+                                    HStack {
+                                        Image(systemName: "checkmark.circle.fill")
+                                        Text(sad)
+                                    }
+                                    
+                                }
+                            }
+                            .padding(.vertical, 30)
+                            Spacer()
+                        }
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(.placeholder, lineWidth: 1)
+                        )
+                        .padding(.bottom, 10)
+                    }
+                    
+                }
+                .padding(.horizontal, 70)
+                .padding(.bottom, 50)
+                .padding(.top, 20)
+            }
+        }.navigationTitle("Community")
+    }
+}
+
+#Preview {
+    CommunityView()
+}
--- TravRec/lib/dataList.swift
+++ TravRec/lib/dataList.swift
@@ -23,6 +23,14 @@ struct travelInterface: Identifiable {
     
 }
 
+struct userInterface: Identifiable {
+    var id: Int
+    var userId: String
+    var userName: String
+    var userProfileImg: String
+    var travelList: [travelInterface]
+}
+
 
 //struct travelListInterface: Identifiable {
 //    var id: UUID
@@ -34,7 +42,7 @@ struct travelInterface: Identifiable {
 //var travel0 = travelListInterface(id: UUID(), title: "영국", startDate: "2023.03.04", endDate: "2023.03.19")
 //var travel1 = travelListInterface(id: UUID(), title: "aㅣ국", startDate: "2023.03.04", endDate: "2023.03.19")
 
-var travel0 = travelInterface(id: UUID(), title: "영국", startDate: "2023.03.04", endDate: "2023.03.19", isPublic: false, titleimg: Image("travrec_logo"), bucketList: [bucket1,bucket2,bucket3,bucket4], happyList: [], sadList: [])
+var travel0 = travelInterface(id: UUID(), title: "영국", startDate: "2023.03.04", endDate: "2023.03.19", isPublic: false, titleimg: Image("travrec_logo"), bucketList: [bucket1,bucket2,bucket3,bucket4], happyList: ["길가다 시장에서 양꼬치 사먹은 것", "지나가는 현지인이랑 스몰톡 해본 것"], sadList: ["축구를 못봐서 아쉬웠다...", "원래 날씨가 좋다던데 딱 내가 갔을 때 비가왔다..ㅜ"])
 
 var travel1 = travelInterface(id: UUID(), title: "미국", startDate: "2023.05.04", endDate: "2023.05.19", isPublic: false, titleimg: Image("travrec_logo"), bucketList:  [bucket1,bucket2,bucket3,bucket4], happyList: [], sadList: [])
 var travel2 = travelInterface(id: UUID(), title: "중국", startDate: "2023.05.04", endDate: "2023.05.19", isPublic: false, titleimg: Image("travrec_logo"), bucketList:  [bucket1,bucket2,bucket3,bucket4], happyList: [], sadList: [])
@@ -55,3 +63,7 @@ var bucket1 = bucketInterface(id: 0, bucketTitle: "현지인과 스몰톡 해보
 var bucket2 = bucketInterface(id: 1, bucketTitle: "비키니 입어보기", bucketImg: "", bucketDesct: "", isComplete: false)
 var bucket3 = bucketInterface(id: 2, bucketTitle: "지나가는 사람 인스타 교환해보기", bucketImg: "", bucketDesct: "", isComplete: false)
 var bucket4 = bucketInterface(id: 3, bucketTitle: "한번도 안 먹어본 음식 먹어보기", bucketImg: "", bucketDesct: "", isComplete: false)
+
+var user1 = userInterface(id: 0, userId: "jhi2359", userName: "Micky", userProfileImg: "micky_profile",travelList: [travel0, travel1, travel2, travel3])
+var user2 = userInterface(id: 1, userId: "negosdc", userName: "pudding", userProfileImg: "pudding_profile",travelList: [travel4, travel5, travel6])
+var user3 = userInterface(id: 2, userId: "esw", userName: "bell", userProfileImg: "bell_profile",travelList: [travel3, travel8, travel10])
